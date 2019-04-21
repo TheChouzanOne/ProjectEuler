e1e590a7431a40e1185b7a40aef6203b96124e5e
adrian <thechouzanone@gmail.com>
2019-04-21 13:15:04 -0700

    Weird VSCode error. Can't fix

--- P005/solution.py
+++ P005/solution.py
@@ -1,10 +1,10 @@
 from time import time
-import numpy as np
+from numpy import lcm
 from functools import reduce
 t0 = time()
 N = 20
 
-ans =reduce(np.lcm, range(1,N+1), 1)
+ans =reduce(lcm, range(1,N+1), 1)
 
 print(ans)
 print("Time: %s"%(time()-t0))
